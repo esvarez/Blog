8714c2def2040478bfdc5f117553e328f51c273a
Erick Suarez <esuarez@esuarez-pro.huawei.net>
2022-01-07 14:41:15 -0600

    Clean repo

--- src/main/java/dev/ericksuarez/roomies/ledger/service/facade/AccountFacade.java
+++
@@ -1,4 +0,0 @@
-package dev.ericksuarez.roomies.ledger.service.facade;
-
-public class AccountFacade {
-}
--- src/main/java/dev/ericksuarez/roomies/ledger/service/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.ericksuarez.roomies.ledger.service.repository;
-
-import dev.ericksuarez.roomies.ledger.service.model.entity.relations.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
-}
